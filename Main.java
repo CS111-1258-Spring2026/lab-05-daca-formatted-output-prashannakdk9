249444c411252a13d4bedfa56ac7b79f77a48de3
prashannakdk9 <prashannakhadka09@gmail.com>
2026-03-16 17:57:36 +0000

    cc

--- Main.java
+++ Main.java
@@ -1,103 +1,102 @@
-// PARTNER NAME:
+// PARTNER NAME: Prashanna khadka
 // PARTNER NAME:
 // CS111 SECTION #:
-// DATE: 
+// DATE: 01/18/2026 
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		//ascii art credit: http://www.oocities.org/spunk1111/bodypart.htm
-		final String ASCII_ART_0 ="    .----.    .----.     ",
-					ASCII_ART_1 = "   (  --  \\  /  --  )    ",
-					ASCII_ART_2 = "          |  |           ",
-					ASCII_ART_3 = "         _/  \\_          ",
-					ASCII_ART_4 = "        (_    _)         ",
-					ASCII_ART_5 = "     ,    `--`    ,      ",
-					ASCII_ART_6 = "     \\'-.______.-'/      ",
-					ASCII_ART_7 = "      \\          /       ",
-					ASCII_ART_8 = "       '.--..--.'        ",
-					ASCII_ART_9 = "         `\"\"\"\"\"`         ",
-					ASCII_CREDIT =" ascii art by: jgs    ";
+public class Main {
 
-		final String TITLE_USA = "UNITED STATES OF AMERICA",
-				TITLE_EAC = "EMPLOYMENT AUTHORIZATION CARD";
-		final String LABEL_SURNAME = "Surname", LABEL_GIVEN_NAME = "Given Name", LABEL_USCIS_NUM = "USCIS#",
-				LABEL_CATEGORY = "Category", LABEL_CARD_NUM = "Card#", LABEL_BIRTH_COUNTRY = "Country of Birth",
-				LABEL_TERMS_CONDITIONS = "Terms and Conditions", LABEL_BIRTH_DATE = "Date of Birth",
-				LABEL_SEX = "Sex", LABEL_VALID_DATE = "Valid From:", LABEL_EXPIRE_DATE = "Card Expires:",
-				LABEL_REENTRY_DISCLAIMER = "NOT VALID FOR REENTRY TO U.S.";
-		
-		//DECLARATION SECTION
-		//complete fields
-		String surname, givenName, category, cardNum, birthCountry, termsAndConditions;
-		char sex;
-		
-		//parts of fields
-		String birthMonth;
-		int uscisNum1, uscisNum2, uscisNum3, birthDay, birthYear,
-			validDay, validMonth, validYear, expireMonth, expireDay, expireYear;
-			
-		//extra vars to reduce messy print statements
-		String uscisNum, dateOfBirth, validDate, expireDate;
-	
-		//INITIALIZATION SECTION
-		//data from EAC database
-		surname = "CHAPETON-LAMAS";
-		givenName = "NERY";
-		category = "C09";
-		cardNum = "SRC9876543210";
-		birthCountry = "Guatemala";
-		termsAndConditions = "None";
-		sex = 'M';
-		
-		birthMonth = "JAN";
-		uscisNum1 = 12;
-		uscisNum2 = 4;
-		uscisNum3 = 789;
-		birthDay = 1;
-		birthYear = 1970;
-		validDay = 2;
-		validMonth = 2;
-		validYear = 2020;
-		expireMonth = 2;
-		expireDay = 2;
-		expireYear = 2022;
-		
-		//additional variables to help cleanup long print's below
-		uscisNum = uscisNum1 + "-" + uscisNum2 + "-" + uscisNum3;
-		dateOfBirth = birthDay + " " + birthMonth + " " + birthYear;
-		validDate = validMonth + "/" + validDay + "/" + validYear;
-		expireDate = expireMonth + "/" + expireDay + "/" + expireYear;
-		
-		//INPUT + CALCULATION SECTION
-		//N/A
-		
-		//OUTPUT SECTION
-		System.out.println("╔══════════════════════════════════════════════════════════════════════╗");
-		System.out.println("║           " + TITLE_USA + "                                   ║");
-		System.out.println("║                               " + TITLE_EAC + "          ║");
-		
-		System.out.println("║                         " + LABEL_SURNAME + "║");
-		System.out.println("║                         " + surname + "║");
-		System.out.println("║" + ASCII_ART_0 + LABEL_GIVEN_NAME + "║");
-		System.out.println("║" + ASCII_ART_1 + givenName + "║");
-		System.out.println("║" + ASCII_ART_2 + LABEL_USCIS_NUM + LABEL_CATEGORY + LABEL_CARD_NUM + "║");
-		System.out.println("║" + ASCII_ART_3 + uscisNum + category + cardNum + "║");
-		
-		System.out.println("║" + ASCII_ART_4 + LABEL_BIRTH_COUNTRY + "║");
-		System.out.println("║" + ASCII_ART_5 + birthCountry + "║");
-		System.out.println("║" + ASCII_ART_6 + LABEL_TERMS_CONDITIONS + "║");
-		System.out.println("║" + ASCII_ART_7 + termsAndConditions + "║");
-		
-		System.out.println("║" + ASCII_ART_8 + LABEL_BIRTH_DATE + LABEL_SEX + "║");
-		System.out.println("║" + ASCII_ART_9 + dateOfBirth + sex + "║");
-		
-		System.out.println("║                         " + LABEL_VALID_DATE + validDate + "║");
-		System.out.println("║                         " + LABEL_EXPIRE_DATE + expireDate + "║");
-		
-		System.out.println("║" + ASCII_CREDIT + LABEL_REENTRY_DISCLAIMER + "║");
-		System.out.println("╚══════════════════════════════════════════════════════════════════════╝");
-		
-	}
+    public static void main(String[] args) {
+
+        // ===== ASCII ART =====
+        final String ASCII_ART_0 = "    .----.    .----.     ",
+                     ASCII_ART_1 = "   (  --  \\  /  --  )    ",
+                     ASCII_ART_2 = "          |  |           ",
+                     ASCII_ART_3 = "         _/  \\_          ",
+                     ASCII_ART_4 = "        (_    _)         ",
+                     ASCII_ART_5 = "     ,    `--`    ,      ",
+                     ASCII_ART_6 = "     \\'-.______.-'/      ",
+                     ASCII_ART_7 = "      \\          /       ",
+                     ASCII_ART_8 = "       '.--..--.'        ",
+                     ASCII_ART_9 = "         `\"\"\"\"\"`         ",
+                     ASCII_CREDIT = " ascii art by: jgs";
+
+        // ===== TITLES =====
+        final String TITLE_USA = "UNITED STATES OF AMERICA";
+        final String TITLE_EAC = "EMPLOYMENT AUTHORIZATION CARD";
+
+        // ===== LABELS =====
+        final String LABEL_SURNAME = "Surname";
+        final String LABEL_GIVEN_NAME = "Given Name";
+        final String LABEL_USCIS = "USCIS#";
+        final String LABEL_CATEGORY = "Category";
+        final String LABEL_CARD = "Card#";
+        final String LABEL_COUNTRY = "Country of Birth";
+        final String LABEL_TERMS = "Terms and Conditions";
+        final String LABEL_DOB = "Date of Birth";
+        final String LABEL_SEX = "Sex";
+        final String LABEL_VALID = "Valid From:";
+        final String LABEL_EXPIRE = "Card Expires:";
+        final String LABEL_DISCLAIMER = "NOT VALID FOR REENTRY TO U.S.";
+
+        // ===== DATA =====
+        String surname = "CHAPETON-LAMAS";
+        String givenName = "NERY";
+        String category = "C09";
+        String cardNum = "SRC9876543210";
+        String birthCountry = "Guatemala";
+        String terms = "None";
+        char sex = 'M';
+
+        int uscis1 = 12, uscis2 = 4, uscis3 = 789;
+        int birthDay = 1, birthYear = 1970;
+        String birthMonth = "JAN";
+
+        int validMonth = 2, validDay = 2, validYear = 2020;
+        int expireMonth = 2, expireDay = 2, expireYear = 2022;
+
+        // ===== FORMATTED VALUES =====
+        String uscisNum = String.format("%03d-%03d-%03d", uscis1, uscis2, uscis3);
+        String dateOfBirth = String.format("%02d %s %4d", birthDay, birthMonth, birthYear);
+        String validDate = String.format("%02d/%02d/%04d", validMonth, validDay, validYear);
+        String expireDate = String.format("%02d/%02d/%04d", expireMonth, expireDay, expireYear);
+
+        final int WIDTH = 70;
+
+        // ===== OUTPUT =====
+        System.out.printf("║%-" + WIDTH + "s║%n", "           " + TITLE_USA);
+        System.out.printf("║%-" + WIDTH + "s║%n", "                               " + TITLE_EAC);
+        System.out.printf("║%-" + WIDTH + "s║%n", "");
+
+        System.out.printf("║%-" + WIDTH + "s║%n", LABEL_SURNAME);
+        System.out.printf("║%-" + WIDTH + "s║%n", surname);
+
+        System.out.printf("║%-" + WIDTH + "s║%n", ASCII_ART_0 + LABEL_GIVEN_NAME);
+        System.out.printf("║%-" + WIDTH + "s║%n", ASCII_ART_1 + givenName);
+
+        System.out.printf("║%-" + WIDTH + "s║%n",
+                ASCII_ART_2 + LABEL_USCIS + "         " + LABEL_CATEGORY + "       " + LABEL_CARD);
+        System.out.printf("║%-" + WIDTH + "s║%n",
+                ASCII_ART_3 + uscisNum + "    " + category + "            " + cardNum);
+
+        System.out.printf("║%-" + WIDTH + "s║%n", ASCII_ART_4 + LABEL_COUNTRY);
+        System.out.printf("║%-" + WIDTH + "s║%n", ASCII_ART_5 + birthCountry);
+
+        System.out.printf("║%-" + WIDTH + "s║%n", ASCII_ART_6 + LABEL_TERMS);
+        System.out.printf("║%-" + WIDTH + "s║%n", ASCII_ART_7 + terms);
+
+        System.out.printf("║%-" + WIDTH + "s║%n",
+                ASCII_ART_8 + LABEL_DOB + "  " + LABEL_SEX);
+        System.out.printf("║%-" + WIDTH + "s║%n",
+                ASCII_ART_9 + dateOfBirth + "    " + sex);
+
+        System.out.printf("║%-" + WIDTH + "s║%n",
+                "                         " + LABEL_VALID + "    " + validDate);
+        System.out.printf("║%-" + WIDTH + "s║%n",
+                "                         " + LABEL_EXPIRE + "  " + expireDate);
+
+        System.out.printf("║%-" + WIDTH + "s║%n",
+                ASCII_CREDIT + "       " + LABEL_DISCLAIMER);
+
+        System.out.println("╚" + "═".repeat(WIDTH) + "╝");
+    }
 }
